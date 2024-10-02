43397ce0e3e6e8a5dae20069414b5525708304b4
pedromand55 <151133698+pedromand55@users.noreply.github.com>
2024-10-02 11:25:42 +0200

    Tilføjet at filter så kun xml filer er tilladte

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+#FileSharingAPI lavet af Lukas (pedromand55 på github)
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 import os
+
+print("FileSharingAPI v1.2")
+print("Starting API...")
 
 app = FastAPI()
 
@@ -8,6 +12,10 @@ app = FastAPI()
 def read_file(file_path: str):
     file_path = os.path.join(os.getcwd(), file_path)
     if os.path.exists(file_path):
-        return {"message": f"File {file_path} exists"}
+        file_extension = os.path.splitext(file_path)[1].lower()
+        if file_extension == ".xml":
+            return {"message": f"File {file_path} exists"}
+        else:
+            return {"error": f"Only XML files are allowed"}
     else:
         return {"error": f"File {file_path} not found"}
